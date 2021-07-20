7fccb143333e2148b6240059ac8a461700dc0435
intey <ziexe0@gmail.com>
2021-07-20 08:50:55 +0300

    test for case when we try to change archived item

--- qs/tests/cache/test_changing.py
+++ qs/tests/cache/test_changing.py
@@ -4,3 +4,13 @@ def test_apply_changes_in_cache(prefilled_infra):
     new_value = "OtherValue"
     cache.alter(node1_idx, new_value)
     assert node1.value == new_value
+
+
+def test_change_deleted_node(prefilled_infra):
+    (memory_db, cache), node1_idx, *_ = prefilled_infra
+    node1 = cache.load(node1_idx)
+    cache.delete(node1_idx)
+
+    new_value = "OtherValue"
+    cache.alter(node1_idx, new_value)
+    assert node1.value != "OtherValue"
